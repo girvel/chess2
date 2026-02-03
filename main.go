747aff1fbdfe57d219c3f54279f857972ab63a23
girvel <widauka@ya.ru>
2026-02-03 15:47:21 +0500

    Draw a pawn

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
+const scale int32 = 4
+
+func loadSprite(filepath string) rl.Texture2D {
+	image := rl.LoadImage(filepath)
+	defer rl.UnloadImage(image)
+	rl.ImageResizeNN(image, image.Width * scale, image.Height * scale)
+	return rl.LoadTextureFromImage(image)
+}
+
 func main() {
 	rl.InitWindow(512, 512, "girvel's chess app")
 	defer rl.CloseWindow()
-
 	rl.SetTargetFPS(60)
+
+	pawn := loadSprite("sprites/pawn.png")
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Hello, raylib!", 10, 10, 20, rl.LightGray)
+		rl.DrawTexture(pawn, 0, 0, rl.White)
 		rl.EndDrawing()
 	}
 }
