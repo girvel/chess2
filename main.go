6e053c4f0acc512512608a67e098cbd0d42a29b3
girvel <widauka@ya.ru>
2026-02-03 15:56:30 +0500

    Draw the board

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 const scale int32 = 4
+const cell_size int32 = 16
+const total_cell_size int32 = scale * cell_size
+
+const w int32 = 8
+const h int32 = 8
 
 func loadSprite(filepath string) rl.Texture2D {
 	image := rl.LoadImage(filepath)
@@ -12,16 +17,32 @@ func loadSprite(filepath string) rl.Texture2D {
 }
 
 func main() {
-	rl.InitWindow(512, 512, "girvel's chess app")
+	rl.InitWindow(w * total_cell_size, h * total_cell_size, "girvel's chess app")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
 
 	pawn := loadSprite("sprites/pawn.png")
+	white := rl.GetColor(0xb5bdbaff)
+	black := rl.GetColor(0x3a373dff)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawTexture(pawn, 0, 0, rl.White)
+			for x := range w {
+				for y := range h {
+					var square_color rl.Color
+					if (x + y) % 2 == 0 {
+						square_color = white
+					} else {
+						square_color = black
+					}
+					rl.DrawRectangle(
+						int32(x) * total_cell_size, int32(y) * total_cell_size,
+						total_cell_size, total_cell_size,
+						square_color,
+					)
+				}
+			}
+			rl.DrawTexture(pawn, 0, 0, rl.White)
 		rl.EndDrawing()
 	}
 }
