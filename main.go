612952d13826b2a8abc6d5b680a8ba1323521edf
girvel <widauka@ya.ru>
2026-02-04 15:42:13 +0500

    Extract io_system

--- main.go
+++ main.go
@@ -3,122 +3,26 @@ package main
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 	chess2 "github.com/girvel/chess2/src"
+	"github.com/girvel/chess2/src/io_system"
 )
 
-const scale int = 6
-const cellSize int = 16
-const totalCellSize int = scale * cellSize
-const windowSize = chess2.BoardSize * totalCellSize
-
-var colorWhiteSquare rl.Color = rl.GetColor(0xedededff)
-var colorBlackSquare rl.Color = rl.GetColor(0x3a373dff)
-var colorWhitePiece rl.Color = rl.GetColor(0xedededff)
-var colorBlackPiece rl.Color = rl.GetColor(0x544747ff)
-var colorSelected rl.Color = rl.GetColor(0xcfa867ff)
-
-func LoadSprite(filepath string) rl.Texture2D {
-	image := rl.LoadImage(filepath)
-	defer rl.UnloadImage(image)
-	rl.ImageResizeNN(image, image.Width * int32(scale), image.Height * int32(scale))
-	return rl.LoadTextureFromImage(image)
-}
-
-func LoadSpriteColored(filepath string) rl.Texture2D {
-	image := rl.LoadImage(filepath)
-	defer rl.UnloadImage(image)
-	rl.ImageColorReplace(image, colorWhitePiece, colorBlackPiece)
-	rl.ImageResizeNN(image, image.Width * int32(scale), image.Height * int32(scale))
-	return rl.LoadTextureFromImage(image)
-}
-
 func main() {
-	rl.InitWindow(int32(windowSize), int32(windowSize), "girvel's chess app")
-	defer rl.CloseWindow()
-	rl.SetTargetFPS(60)
-
+	io_system.Init()
+	defer io_system.Deinit()
 	board := chess2.EmptyBoard()
-
-	sprites := []rl.Texture2D{
-		LoadSprite("sprites/none.png"),
-		LoadSprite("sprites/pawn.png"),
-		LoadSpriteColored("sprites/pawn.png"),
-		LoadSprite("sprites/knight.png"),
-		LoadSpriteColored("sprites/knight.png"),
-		LoadSprite("sprites/bishop.png"),
-		LoadSpriteColored("sprites/bishop.png"),
-		LoadSprite("sprites/rook.png"),
-		LoadSpriteColored("sprites/rook.png"),
-		LoadSprite("sprites/queen.png"),
-		LoadSpriteColored("sprites/queen.png"),
-		LoadSprite("sprites/king.png"),
-		LoadSpriteColored("sprites/king.png"),
-	}
-
-	moveSprite := LoadSprite("sprites/move.png")
-	winSprite := LoadSprite("sprites/win.png")
-	lossSprite := LoadSprite("sprites/loss.png")
-
-	var selectedX, selectedY int
-	var potentialMoves []chess2.Move
-	isSelected := false
 	ai := chess2.CreateAi(*board)
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-		for x := range chess2.BoardSize {
-			for y := range chess2.BoardSize {
-				var squareColor rl.Color
-				switch {
-				case isSelected && x == selectedX && y == selectedY:
-					squareColor = colorSelected
-				case (x + y) % 2 == 0:
-					squareColor = colorWhiteSquare
-				default:
-					squareColor = colorBlackSquare
-				}
+	for {
+		io_system.Draw(*board)
 
-				render_x := int32(x * totalCellSize)
-				render_y := int32(y * totalCellSize)
-				rl.DrawRectangle(
-					render_x, render_y,
-					int32(totalCellSize), int32(totalCellSize),
-					squareColor,
-				)
-				
-				piece := *board.At(x, y)
-				if piece != chess2.PieceNone {
-					rl.DrawTexture(sprites[piece], render_x, render_y, rl.White)
-				}
-			}
+		playerMove, shouldClose := io_system.ReadInput(*board)
+		if playerMove != nil {
+			board.Move(*playerMove)
+			ai.PushMove(*playerMove)
 		}
 
-		if isSelected {
-			for _, m := range potentialMoves {
-				rl.DrawTexture(
-					moveSprite,
-					int32(m.X2 * totalCellSize), int32(m.Y2 * totalCellSize),
-					rl.White,
-				)
-			}
-		}
-
-		if board.Winner != chess2.SideNone {
-			var texture rl.Texture2D
-			switch board.Winner {
-			case chess2.SideWhite: texture = winSprite;
-			case chess2.SideBlack: texture = lossSprite;
-			}
-
-			rl.DrawTexture(
-				texture,
-				(int32(windowSize) - texture.Width) / 2, (int32(windowSize) - texture.Height) / 2,
-				rl.White,
-			)
-		}
-		rl.EndDrawing()
-
-		if board.Winner != chess2.SideNone {
-			continue
+		if shouldClose {
+			break
 		}
 
 		if board.Turn == chess2.SideBlack {
@@ -126,36 +30,6 @@ func main() {
 				rl.TraceLog(rl.LogInfo, "AI: %s", m)
 				board.Move(*m)
 			}
-			continue
-		}
-
-		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
-			x := int(rl.GetMouseX()) / totalCellSize
-			y := int(rl.GetMouseY()) / totalCellSize
-
-			if isSelected {
-				move := chess2.NewMove(selectedX, selectedY, x, y)
-				if board.IsMoveLegal(move) {
-					board.Move(move)
-					ai.PushMove(move)
-				}
-				isSelected = false
-			} else {
-				if board.At(x, y).Is(board.Turn) {
-					selectedX = x
-					selectedY = y
-					isSelected = true
-					potentialMoves = board.GetMoves(x, y)
-				}
-			}
 		}
 	}
-
-	for _, sprite := range sprites {
-		rl.UnloadTexture(sprite)
-	}
-
-	rl.UnloadTexture(moveSprite)
-	rl.UnloadTexture(winSprite)
-	rl.UnloadTexture(lossSprite)
 }
---
+++ src/io_system/impl.go
@@ -0,0 +1,159 @@
+package io_system
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	chess2 "github.com/girvel/chess2/src"
+)
+
+const scale int = 6
+const cellSize int = 16
+const totalCellSize int = scale * cellSize
+const windowSize = chess2.BoardSize * totalCellSize
+
+var colorWhiteSquare rl.Color = rl.GetColor(0xedededff)
+var colorBlackSquare rl.Color = rl.GetColor(0x3a373dff)
+var colorWhitePiece rl.Color = rl.GetColor(0xedededff)
+var colorBlackPiece rl.Color = rl.GetColor(0x544747ff)
+var colorSelected rl.Color = rl.GetColor(0xcfa867ff)
+
+var pieceSprites []rl.Texture2D
+var moveSprite, winSprite, lossSprite rl.Texture2D
+
+var selectedX, selectedY int
+var potentialMoves []chess2.Move
+var isSelected = false
+
+func Init() {
+	rl.InitWindow(int32(windowSize), int32(windowSize), "girvel's chess app")
+	rl.SetTargetFPS(60)
+
+	pieceSprites = []rl.Texture2D{
+		loadSprite("sprites/none.png"),
+		loadSprite("sprites/pawn.png"),
+		loadSpriteColored("sprites/pawn.png"),
+		loadSprite("sprites/knight.png"),
+		loadSpriteColored("sprites/knight.png"),
+		loadSprite("sprites/bishop.png"),
+		loadSpriteColored("sprites/bishop.png"),
+		loadSprite("sprites/rook.png"),
+		loadSpriteColored("sprites/rook.png"),
+		loadSprite("sprites/queen.png"),
+		loadSpriteColored("sprites/queen.png"),
+		loadSprite("sprites/king.png"),
+		loadSpriteColored("sprites/king.png"),
+	}
+
+	moveSprite = loadSprite("sprites/move.png")
+	winSprite = loadSprite("sprites/win.png")
+	lossSprite = loadSprite("sprites/loss.png")
+}
+
+func Draw(board chess2.Board) {
+	rl.BeginDrawing()
+	for x := range chess2.BoardSize {
+		for y := range chess2.BoardSize {
+			var squareColor rl.Color
+			switch {
+			case isSelected && x == selectedX && y == selectedY:
+				squareColor = colorSelected
+			case (x + y) % 2 == 0:
+				squareColor = colorWhiteSquare
+			default:
+				squareColor = colorBlackSquare
+			}
+
+			render_x := int32(x * totalCellSize)
+			render_y := int32(y * totalCellSize)
+			rl.DrawRectangle(
+				render_x, render_y,
+				int32(totalCellSize), int32(totalCellSize),
+				squareColor,
+			)
+			
+			piece := *board.At(x, y)
+			if piece != chess2.PieceNone {
+				rl.DrawTexture(pieceSprites[piece], render_x, render_y, rl.White)
+			}
+		}
+	}
+
+	if isSelected {
+		for _, m := range potentialMoves {
+			rl.DrawTexture(
+				moveSprite,
+				int32(m.X2 * totalCellSize), int32(m.Y2 * totalCellSize),
+				rl.White,
+			)
+		}
+	}
+
+	if board.Winner != chess2.SideNone {
+		var texture rl.Texture2D
+		switch board.Winner {
+		case chess2.SideWhite: texture = winSprite;
+		case chess2.SideBlack: texture = lossSprite;
+		}
+
+		rl.DrawTexture(
+			texture,
+			(int32(windowSize) - texture.Width) / 2, (int32(windowSize) - texture.Height) / 2,
+			rl.White,
+		)
+	}
+	rl.EndDrawing()
+}
+
+func ReadInput(board chess2.Board) (*chess2.Move, bool) {
+	shouldClose := rl.WindowShouldClose()
+	if board.Winner != chess2.SideNone || board.Turn == chess2.SideBlack {
+		return nil, shouldClose
+	}
+
+	if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+		x := int(rl.GetMouseX()) / totalCellSize
+		y := int(rl.GetMouseY()) / totalCellSize
+
+		if isSelected {
+			isSelected = false
+			move := chess2.NewMove(selectedX, selectedY, x, y)
+			if board.IsMoveLegal(move) {
+				return &move, shouldClose
+			}
+		} else {
+			if board.At(x, y).Is(board.Turn) {
+				selectedX = x
+				selectedY = y
+				isSelected = true
+				potentialMoves = board.GetMoves(x, y)
+			}
+		}
+	}
+
+	return nil, shouldClose
+}
+
+func Deinit() {
+	for _, sprite := range pieceSprites {
+		rl.UnloadTexture(sprite)
+	}
+
+	rl.UnloadTexture(moveSprite)
+	rl.UnloadTexture(winSprite)
+	rl.UnloadTexture(lossSprite)
+	rl.CloseWindow()
+}
+
+func loadSprite(filepath string) rl.Texture2D {
+	image := rl.LoadImage(filepath)
+	defer rl.UnloadImage(image)
+	rl.ImageResizeNN(image, image.Width * int32(scale), image.Height * int32(scale))
+	return rl.LoadTextureFromImage(image)
+}
+
+func loadSpriteColored(filepath string) rl.Texture2D {
+	image := rl.LoadImage(filepath)
+	defer rl.UnloadImage(image)
+	rl.ImageColorReplace(image, colorWhitePiece, colorBlackPiece)
+	rl.ImageResizeNN(image, image.Width * int32(scale), image.Height * int32(scale))
+	return rl.LoadTextureFromImage(image)
+}
